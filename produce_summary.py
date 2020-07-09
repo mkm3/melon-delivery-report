a7b2cab1620e31825add4adf09d0a72150f7aab3
Michelle Macaraeg <macaraegm@gmail.com>
2020-07-08 22:48:52 -0700

    Added Comments for Clarity

--- produce_summary.py
+++ produce_summary.py
@@ -5,20 +5,27 @@ def better_report(day_number, path):
 
     print()
     print("Day", day_number)
+    # displays the day (day_number) of the delivery
 
     the_file = open(path)
+    # assigning variable to our open function, passing through produce report (path)
 
     for line in the_file:
+    # iterating through each line of the produce report
+
         line = line.rstrip()
+        #removes the whitespace to the right of the string
+
         words = line.split('|')
+        #item is created by split string when "|" is located in line
 
         melon = words[0]
         count = words[1]
         amount = words[2]
+        #variables for our print statement below
 
         print("   Delivered {} {}s for total of ${}".format(
             count, melon, amount))
-    the_file.close()
 
     the_file.close()
 
